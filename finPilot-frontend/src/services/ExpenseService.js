599aff9f38dea005008f2f51ff8b5ec2fadeee7e
Onkar Shesh <onkarshesh16@gmail.com>
2026-07-31 05:25:01 +0530

    Update backend API URL

--- finPilot-frontend/src/services/ExpenseService.js
+++ finPilot-frontend/src/services/ExpenseService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://finpilot-ai-4ygw.onrender.com/api";
 
 const getToken = () => {
     return localStorage.getItem("token");
